11e28dff626c31d634fbc0879201031bdda6533b
Elvira Fuente <hebebicho@gmail.com>
2019-07-12 17:04:34 +0200

    fix menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import './styles.scss';
 import Decoration from '../Decoration';
 import Menu from '../Menu';
+import Arrow from '../../images/arrow.png'
 
 function Header(props) {
-    const {openMenu, menuDisplayed} = props;
+    const { openMenu, menuDisplayed } = props;
     return (
         <header className="page__header">
             <Decoration />
-            <div className="page__hero-container">
-                <p className="header__hero">Hola!</p>
-                <p className="header__hero">Soy Elvira Fuente,</p>
-                <h1 className="header__hero sub">Programadora Front End.</h1>
+            <div className="page__header-container">
+                <div className="page__hero-container">
+                    <p className="header__hero">Hola!</p>
+                    <p className="header__hero">Soy Elvira Fuente,</p>
+                    <h1 className="header__hero sub">Programadora Front End.</h1>
+                </div>
+                <img src={Arrow} className="header__ico"/>
             </div>
-            <Menu openMenu={openMenu} menuDisplayed={menuDisplayed}/>
+            <Menu openMenu={openMenu} menuDisplayed={menuDisplayed} />
         </header>
     )
 }
